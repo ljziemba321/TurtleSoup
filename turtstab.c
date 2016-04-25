f79a1996fcfc346c88d756c9d2ca3220d801bfd8
Hunter Easterday <hpeasterday@gmail.com>
2016-04-25 17:07:02 -0500

    Variable assignment now checked

--- turtstab.c
+++ turtstab.c
@@ -3,7 +3,7 @@ typedef entry *entrylink;
 struct entrytype
 {
 	char name[20];
-	int type; //1 is turtle, 2 is variable, 3 is instinct
+	int type; //1 is turtle, 2 is variable, 3 is instinct, 4 is variable with value in it
 	entrylink ptr;
 };
 
@@ -43,10 +43,10 @@ void addTab(char *str, int newtype)
 	else
 	{	if(typecheck == 1)
 			printf("Variable %s already in table of TURTLE type, redeclaration not allowed\n", str);
-		if(typecheck == 2)
+		if(typecheck == 2 || typecheck == 4)
 			printf("Variable %s already in table of INTEGER type, redeclaration not allowed\n", str);
 		if(typecheck == 3)
-			printf("Variable %s already in table of INSTINCT type, redeclaration not allowed\n", str);		
+			printf("Variable %s already in table of INSTINCT type, redeclaration not allowed\n", str);	
 		exit(1);
 	}
 }
@@ -65,6 +65,34 @@ int inTab(char *str)
 	return 0;
 }
 
+void varsetVal(char *str)
+{
+	entrylink tmp;
+	tmp = head;
+	while(tmp != NULL)
+	{
+		if (strcmp(tmp->name, str) == 0)
+		{
+			if(tmp->type == 2)
+			{
+				tmp->type = 4;
+				return void;
+			}
+			else
+			{
+				printf("Improper use of variable trying to load integer value into non-integer variable: %s\n", tmp->name);
+				exit(1);
+			}
+		}		
+		else
+			tmp = tmp->ptr;
+	}
+	printf("Improper use of variable trying to load integer value into non-declared variable: %s\n", str);
+	exit(1);
+}
+
+
+}
 void printTab()
 {
 	entrylink tmp;
